aebc21ad92d90a855ea979be26c5bb414a5d8135
Nora Söderlund <nora.soderlund@polestar.com>
2023-04-09 21:01:10 +0200

    Fixed user key query

--- src/controllers/users/keys/getUserKeyByCode.ts
+++ src/controllers/users/keys/getUserKeyByCode.ts
@@ -2,5 +2,5 @@ import { User } from "../../../models/user";
 import { UserKey } from "../../../models/userKey";
 
 export async function getUserKeyByCode(database: D1Database, code: string): Promise<UserKey> {
-    return await database.prepare("SELECT * FROM user_keys WHERE code = ?").bind(code).first();
+    return await database.prepare("SELECT * FROM user_keys WHERE id = ?").bind(code).first();
 };
