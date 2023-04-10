46c222e8fdfe120cb6571dce1e6c2f55df266175
Nora Söderlund <norasoderlund@icloud.com>
2023-04-10 11:43:47 +0200

    Added dot folder exceptions

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+    test: {
+        exclude: ['node_modules', 'dist', '.idea', '.git', '.cache', '.wrangler', '.github']
+    }
 });
