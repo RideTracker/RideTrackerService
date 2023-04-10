f3c2465188d5f729b82b3eb05069aaa34db61fff
Nora Söderlund <nora.soderlund@polestar.com>
2023-04-10 14:26:31 +0200

    Updated service name

--- tests/utils/response.ts
+++ tests/utils/response.ts
@@ -1,5 +1,5 @@
 export async function getResponse(method: string, url: string, body: any = null): Promise<any> {
-    const response = await fetch("https://test-service-staging.norasoderlund9092.workers.dev" + url, {
+    const response = await fetch("https://service-staging.norasoderlund9092.workers.dev" + url, {
         method,
         headers: {
             "Content-Type": "application/json"
