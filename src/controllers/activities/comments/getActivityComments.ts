c08ae5cfa7d9c14da5178b21f3449a18e214efb2
Nora Söderlund <nora.soderlund@polestar.com>
2023-04-14 12:34:58 +0200

    Updated queries

--- src/controllers/activities/comments/getActivityComments.ts
+++ src/controllers/activities/comments/getActivityComments.ts
@@ -3,10 +3,5 @@ import { ActivityComment } from "../../../models/activityComment";
 import { ActivitySummary } from "../../../models/activitySummary";
 
 export async function getActivityComments(database: D1Database, activity: string): Promise<ActivityComment[] | null> {
-    const { results } = await database.prepare("SELECT * FROM activity_comments WHERE activity = ?").bind(activity).all();
-
-    if(!results)
-        return null;
-
-    return results as ActivityComment[];
+    return (await database.prepare("SELECT * FROM activity_comments WHERE activity = ?").bind(activity).all<ActivityComment>()).results ?? null;
 };
--- src/controllers/activities/getActivities.ts
+++ src/controllers/activities/getActivities.ts
@@ -1,10 +1,5 @@
 import { Activity } from "../../models/activity";
 
 export async function getActivities(database: D1Database): Promise<Activity[] | null> {
-    const { results } = await database.prepare("SELECT * FROM activities").all();
-
-    if(!results)
-        return null;
-
-    return results as Activity[];
+    return (await database.prepare("SELECT * FROM activities").all<Activity>()).results ?? null;
 };
