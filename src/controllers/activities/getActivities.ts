b06178ec2c03e6d8e6c942877311696ec7856f35
Nora Söderlund <norasoderlund@icloud.com>
2023-04-15 19:52:23 +0200

    Updated activities query

--- src/controllers/activities/getActivities.ts
+++ src/controllers/activities/getActivities.ts
@@ -1,5 +1,5 @@
 import { Activity } from "../../models/activity";
 
 export async function getActivities(database: D1Database): Promise<Activity[] | null> {
-    return (await database.prepare("SELECT * FROM activities").all<Activity>()).results ?? null;
+    return (await database.prepare("SELECT * FROM activities ORDER BY timestamp DESC").all<Activity>()).results ?? null;
 };
