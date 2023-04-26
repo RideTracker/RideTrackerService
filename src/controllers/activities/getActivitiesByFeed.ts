226c29471c9a6a59d2f3a361d49ff743cd713b67
Nora Söderlund <nora.soderlund@polestar.com>
2023-04-26 18:09:03 +0200

    Added search filter to feed

---
+++ src/controllers/activities/getActivitiesByFeed.ts
@@ -0,0 +1,22 @@
+import { Activity } from "../../models/activity";
+
+export async function getActivitiesByFeed(database: D1Database, search?: string, order?: string, timeline?: string): Promise<Activity[] | null> {
+    if(search?.length) {
+        const query = await database.prepare(
+            "SELECT * FROM activities" +
+            " INNER JOIN users ON activities.user = users.id" +
+            " WHERE" +
+            " (LOWER(activities.title) LIKE '%' || LOWER(?1) || '%')" +
+            " OR (LOWER(activities.description) LIKE '%' || LOWER(?1) || '%')" +
+            " OR (LOWER(users.firstname) LIKE '%' || LOWER(?1) || '%')" +
+            " OR (LOWER(users.lastname) LIKE '%' || LOWER(?1) || '%')" +
+            " ORDER BY activities.timestamp DESC"
+            ).bind(search).all<Activity>();
+    
+        return query.results ?? null;
+    }
+
+    const query = await database.prepare("SELECT * FROM activities ORDER BY timestamp DESC").all<Activity>();
+
+    return query.results ?? null;
+};
--- src/routes/feed.ts
+++ src/routes/feed.ts
@@ -1,7 +1,29 @@
 import { getActivities } from "../controllers/activities/getActivities";
+import { getActivitiesByFeed } from "../controllers/activities/getActivitiesByFeed";
+
+const feedRequestSchema = {
+    params: {
+        search: {
+            type: "string",
+            required: false
+        },
+        
+        order: {
+            type: "string",
+            required: false
+        },
+        
+        timeline: {
+            type: "string",
+            required: false
+        }
+    }  
+};
 
 export async function handleFeedRequest(request: Request, env: Env) {
-    const activities = await getActivities(env.DATABASE);
+    const { search, order, timeline } = request.params;
+
+    const activities = await getActivitiesByFeed(env.DATABASE, search, order, timeline);
 
     if(!activities)
         return Response.json({ success: false });
