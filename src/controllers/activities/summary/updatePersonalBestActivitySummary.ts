27dabf8748823015495dc657708133f2d9730b9a
Nora Söderlund <nora.soderlund@polestar.com>
2023-04-26 22:05:03 +0200

    Update summary query

--- src/controllers/activities/summary/updatePersonalBestActivitySummary.ts
+++ src/controllers/activities/summary/updatePersonalBestActivitySummary.ts
@@ -5,7 +5,7 @@ function updatePersonalBestActivitySummaryColumn(database: D1Database, user: str
     return database.prepare(
         "UPDATE activity_summary SET " + column + "_personal_best = NULL WHERE " + column + "_personal_best IS NULL AND activity_summary.id = (SELECT activities.id FROM activities JOIN activity_summary ON activities.id = activity_summary.id WHERE user = ?1); " +
         "UPDATE activity_summary SET " + column + "_personal_best = 1 WHERE activity_summary.id = (SELECT activities.id FROM activities JOIN activity_summary ON activities.id = activity_summary.id WHERE activities.user = ?1 ORDER BY activity_summary." + column + " DESC LIMIT 1)"
-    ).bind(user).first<string>("activity_summary.id");
+    ).bind(user).run();
 };
 
 export async function updatePersonalBestActivitySummary(database: D1Database, user: string): Promise<void> {
