8dc8acc663bcb40ed040f14d004f4c058030de26
Nora Söderlund <nora.soderlund@polestar.com>
2023-04-26 18:30:09 +0200

    Added area search for activities

--- src/controllers/activities/getActivitiesByFeed.ts
+++ src/controllers/activities/getActivitiesByFeed.ts
@@ -4,12 +4,14 @@ export async function getActivitiesByFeed(database: D1Database, search?: string,
     if(search?.length) {
         const query = await database.prepare(
             "SELECT activities.* FROM activities" +
+            " INNER JOIN activity_summary ON activities.id = activity_summary.id" +
             " INNER JOIN users ON activities.user = users.id" +
             " WHERE" +
             " (LOWER(activities.title) LIKE '%' || LOWER(?1) || '%')" +
             " OR (LOWER(activities.description) LIKE '%' || LOWER(?1) || '%')" +
-            " OR (LOWER(users.firstname) LIKE '%' || LOWER(?1) || '%')" +
-            " OR (LOWER(users.lastname) LIKE '%' || LOWER(?1) || '%')" +
+            " OR (LOWER(users.firstname) || ' ' || LOWER(users.lastname) LIKE '%' || LOWER(?1) || '%')" +
+            " OR (LOWER(activity_summary.start_area) LIKE '%' || LOWER(?1) || '%')" +
+            " OR (LOWER(activity_summary.finish_area) LIKE '%' || LOWER(?1) || '%')" +
             " ORDER BY activities.timestamp DESC"
             ).bind(search).all<Activity>();
     
