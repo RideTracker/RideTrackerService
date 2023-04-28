ac14e30a1d9b6ad9b547ba21fae28fb179a07a2b
Nora Söderlund <nora.soderlund@polestar.com>
2023-04-28 19:27:48 +0200

    Added mock comments

--- tests/data.test.ts
+++ tests/data.test.ts
@@ -1,6 +1,7 @@
 import { describe, test } from "vitest";
 import { createUser } from "./controllers/users/createUser";
 import { createActivity } from "./controllers/activities/createActivity";
+import { createActivityComment } from "./controllers/activities/comments/createActivityComment";
 
 describe("populate mock data", async () => {
     test("create users and populate content", async () => {
@@ -11,10 +12,27 @@ describe("populate mock data", async () => {
             users.push(result);
         }
 
+        const activities = [];
         for(let index = 0; index < 15; index++) {
             const activityResult = await createActivity(users[Math.floor(Math.random() * users.length)].key);
             
+            if(!activityResult) {
+                console.warn("failed", activityResult);
+
+                continue;
+            }
+            
             console.log(index, activityResult);
+
+            activities.push(activityResult.activity.id);
         }
+
+        for(let index = 0; index < 20; index++) {
+            const activity = activities[Math.floor(Math.random() * activities.length)];
+
+            const commentResult = await createActivityComment(users[Math.floor(Math.random() * users.length)].key, activity);
+            
+            console.log(index, commentResult);
+        };
     }, { timeout: 0 });
 });
