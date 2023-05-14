6ad76966ccaadd7f354b9f68bb3f5c5ec10a61ab
Nora Söderlund <norasoderlund@icloud.com>
2023-05-14 17:53:29 +0200

    Updated CORS

--- src/index.ts
+++ src/index.ts
@@ -116,6 +116,7 @@ export default {
         
         response.headers.set("Access-Control-Allow-Origin", "*");
         response.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
+        response.headers.set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
 
         return response;
     }
