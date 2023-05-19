e3afaf4100fba1437dde2f03188e2da51335d14d
Nora Söderlund <norasoderlund@icloud.com>
2023-05-19 22:29:28 +0200

    Add live tests

---
+++ tests/endpoints.test.ts
@@ -0,0 +1,41 @@
+import { describe, expect, test } from "vitest";
+import Client from "@ridetracker/ridetrackerclient";
+import ping from "@ridetracker/ridetrackerclient/dist/controllers/ping";
+
+// @ts-ignore
+const SERVICE_API = import.meta.env.VITE_SERVICE_API;
+
+// @ts-ignore
+if(import.meta.env.VITE_GITHUB_SHA) {
+    describe("github", async () => {
+        test("verifying github sha", async () => {
+            while(true) {
+                try {
+                    const response = await fetch(`${SERVICE_API}/staging/github`);
+                    const result = await response.json();
+
+                    // @ts-ignore
+                    if(result.sha === import.meta.env.VITE_GITHUB_SHA)
+                        break;
+
+                    await new Promise((resolve) => setTimeout(resolve, 5000));
+                }
+                catch {
+                    continue;
+                }
+            }
+        }, 60000);
+    });
+}
+
+describe("client", () => {
+    test("ping", async () => {
+        const client = new Client(SERVICE_API);
+
+        const pingResponse = await ping(client);
+
+        console.log(pingResponse);
+
+        expect(pingResponse.ping).toBe("pong");
+    });
+});
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import { defineConfig } from "vite";
 export default defineConfig({
     test: {
         exclude: ['node_modules', 'dist', '.idea', '.git', '.cache', '.wrangler', '.github']
+    },
+    define: {
+      'import.meta.vitest': false
     }
 });
