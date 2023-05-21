d9ceb8331a72daa60bfe2ef15c131408ba3d8ba3
Nora Söderlund <norasoderlund@icloud.com>
2023-05-21 15:06:16 +0200

    Update schema

--- database.sql
+++ database.sql
@@ -1,5 +1,5 @@
 DROP TABLE IF EXISTS "activities";
-CREATE TABLE IF NOT EXISTS "activities" (
+CREATE TABLE "activities" (
 	"id"	varchar(255) NOT NULL,
 	"user"	varchar(255) NOT NULL,
 	"title"	varchar(255) DEFAULT NULL,
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS "activities" (
 );
 
 DROP TABLE IF EXISTS "activity_comments";
-CREATE TABLE IF NOT EXISTS "activity_comments" (
+CREATE TABLE "activity_comments" (
 	"id"	varchar(255) NOT NULL,
 	"activity"	varchar(255) NOT NULL,
 	"parent"	varchar(255) DEFAULT NULL,
@@ -20,7 +20,7 @@ CREATE TABLE IF NOT EXISTS "activity_comments" (
 );
 
 DROP TABLE IF EXISTS "activity_likes";
-CREATE TABLE IF NOT EXISTS "activity_likes" (
+CREATE TABLE "activity_likes" (
 	"id"	varchar(255) NOT NULL,
 	"activity"	varchar(255) NOT NULL,
 	"user"	varchar(255) NOT NULL,
@@ -29,7 +29,7 @@ CREATE TABLE IF NOT EXISTS "activity_likes" (
 );
 
 DROP TABLE IF EXISTS "activity_summary";
-CREATE TABLE IF NOT EXISTS "activity_summary" (
+CREATE TABLE "activity_summary" (
 	"id"	varchar(255) NOT NULL,
 	"start_area"	varchar(255) DEFAULT NULL,
 	"finish_area"	varchar(255) DEFAULT NULL,
@@ -45,40 +45,9 @@ CREATE TABLE IF NOT EXISTS "activity_summary" (
 	PRIMARY KEY("id")
 );
 
-DROP TABLE IF EXISTS "avatars";
-CREATE TABLE IF NOT EXISTS "avatars" (
-	"id"	varchar(255) NOT NULL,
-	"name"	varchar(255) NOT NULL,
-	"type"	varchar(255) NOT NULL,
-	"image"	varchar(255) NOT NULL,
-	"timestamp"	bigint NOT NULL,
-	PRIMARY KEY("id")
-);
-
-DROP TABLE IF EXISTS "avatar_images";
-CREATE TABLE IF NOT EXISTS "avatar_images" (
-	"id"	varchar(255) NOT NULL,
-	"avatar"	varchar(255) NOT NULL,
-	"image"	varchar(255) NOT NULL,
-	"index"	int NOT NULL,
-	"color_type"	varchar(255) DEFAULT NULL,
-	"timestamp"	bigint NOT NULL,
-	PRIMARY KEY("id")
-);
-
-DROP TABLE IF EXISTS "avatar_colors";
-CREATE TABLE IF NOT EXISTS "avatar_colors" (
-	"id"	varchar(255) NOT NULL,
-	"avatar"	varchar(255) NOT NULL,
-	"type"	varchar(255) NOT NULL,
-	"index"	int NOT NULL,
-	"default_color"	varchar(16) DEFAULT NULL,
-	"timestamp"	bigint NOT NULL,
-	PRIMARY KEY("id")
-);
 
 DROP TABLE IF EXISTS "bikes";
-CREATE TABLE IF NOT EXISTS "bikes" (
+CREATE TABLE "bikes" (
 	"id"	varchar(255) NOT NULL,
 	"user"	varchar(255) NOT NULL,
 	"name"	varchar(255) NOT NULL,
@@ -88,10 +57,8 @@ CREATE TABLE IF NOT EXISTS "bikes" (
 	PRIMARY KEY("id")
 );
 
-INSERT INTO "bikes" VALUES ('844d20e9-a253-415e-9f5d-ae86a4c21947','95c00e4b-607e-43d3-b871-4661ebc2cedd','Vittoria','Battaglin C11','https://i.imgur.com/Lol0MH3.jpg',1678313271079.0);
-
 DROP TABLE IF EXISTS "bike_summary";
-CREATE TABLE IF NOT EXISTS "bike_summary" (
+CREATE TABLE "bike_summary" (
 	"id"	varchar(255) NOT NULL,
 	"rides"	int NOT NULL,
 	"distance"	double NOT NULL,
@@ -100,10 +67,8 @@ CREATE TABLE IF NOT EXISTS "bike_summary" (
 	PRIMARY KEY("id")
 );
 
-INSERT INTO "bike_summary" VALUES ('844d20e9-a253-415e-9f5d-ae86a4c21947',1,7.3,33.0,1678553980492);
-
 DROP TABLE IF EXISTS "bike_images";
-CREATE TABLE IF NOT EXISTS "bike_images" (
+CREATE TABLE "bike_images" (
 	"id"	varchar(255) NOT NULL,
 	"bike"	varchar(255) NOT NULL,
 	"timestamp"	bigint NOT NULL DEFAULT '0',
@@ -111,7 +76,7 @@ CREATE TABLE IF NOT EXISTS "bike_images" (
 );
 
 DROP TABLE IF EXISTS "users";
-CREATE TABLE IF NOT EXISTS "users" (
+CREATE TABLE "users" (
 	"id"	varchar(255) NOT NULL,
 	"email"	varchar(255) NOT NULL,
 	"password"	varchar(255) NOT NULL,
@@ -122,12 +87,8 @@ CREATE TABLE IF NOT EXISTS "users" (
 	PRIMARY KEY("id")
 );
 
-INSERT INTO "users" VALUES ('256264be-d8ac-4e41-9099-f6d96c50ba3c','norasoderlund@icloud.com2','ues1NuDbbXuwZP0AqN9dkz3NurYY1eIo0IlUsOgkcHg=.Av8nleF1FsnhdWSUgrf6OQ==','Nora','Summerswille','https://i.imgur.com/dIV7nwq.png',1678613688440.0);
-INSERT INTO "users" VALUES ('59174df4-1b1b-4d5f-9d6a-79f23a6b9e69','norasoderlund2@icloud.com','ues1NuDbbXuwZP0AqN9dkz3NurYY1eIo0IlUsOgkcHg=.Av8nleF1FsnhdWSUgrf6OQ==','Nora','Söderlund',NULL,1678485977894.0);
-INSERT INTO "users" VALUES ('95c00e4b-607e-43d3-b871-4661ebc2cedd','norasoderlund@icloud.com','ues1NuDbbXuwZP0AqN9dkz3NurYY1eIo0IlUsOgkcHg=.Av8nleF1FsnhdWSUgrf6OQ==','Nora','Söderlund','https://avatars.githubusercontent.com/u/78360666?v=4',1678300150558.0);
-
 DROP TABLE IF EXISTS "user_avatars";
-CREATE TABLE IF NOT EXISTS "user_avatars" (
+CREATE TABLE "user_avatars" (
 	"id"	varchar(255) NOT NULL,
 	"user"	varchar(255) NOT NULL,
 	"image"	varchar(255) NOT NULL,
@@ -137,7 +98,7 @@ CREATE TABLE IF NOT EXISTS "user_avatars" (
 );
 
 DROP TABLE IF EXISTS "user_follows";
-CREATE TABLE IF NOT EXISTS "user_follows" (
+CREATE TABLE "user_follows" (
 	"id"	varchar(255) NOT NULL,
 	"user"	varchar(255) NOT NULL,
 	"follow"	varchar(255) NOT NULL,
@@ -145,16 +106,17 @@ CREATE TABLE IF NOT EXISTS "user_follows" (
 	PRIMARY KEY("id")
 );
 
-DROP TABLE IF EXISTS "user_keys";
-CREATE TABLE IF NOT EXISTS "user_keys" (
+DROP TABLE IF EXISTS "tokens";
+CREATE TABLE "tokens" (
 	"id"	varchar(255) NOT NULL,
-	"user"	varchar(255) NOT NULL,
+	"key"	varchar(255) NOT NULL,
+	"user"	varchar(255) DEFAULT NULL,
 	"timestamp"	double NOT NULL,
 	PRIMARY KEY("id")
 );
 
 DROP TABLE IF EXISTS "user_verifications";
-CREATE TABLE IF NOT EXISTS "user_verifications" (
+CREATE TABLE "user_verifications" (
 	"id"	varchar(255) NOT NULL,
 	"user"	varchar(255) NOT NULL,
 	"code"	varchar(6) NOT NULL,
