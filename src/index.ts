31af2e39823db84112aca924505ec89adcd4fa1b
Nora Söderlund <norasoderlund@icloud.com>
2023-05-27 18:58:47 +0200

    Fixed error handling

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import { triggerAlarm } from "./controllers/alarms/triggerAlarm";
 import createRouter from "./domains/router";
-import D1Error from "./models/D1Error";
 
 const router = createRouter();
 
@@ -22,33 +21,32 @@ export default {
         try {
             const response = await getRequest(request, env, context);
 
-            if(response.status >= 200 && response.status <= 299) { 
-                response.headers.set("Access-Control-Allow-Origin", "*");
-                response.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
-                response.headers.set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-            }
-            else
-                context.waitUntil(new Promise(async (resolve) => {
+            if(response.status < 200 || response.status > 299) { 
+                context.waitUntil(new Promise<void>(async (resolve) => {
                     const text = await response.text();
 
-                    if(text.length)
-                        await triggerAlarm(env, "Unsuccessful Status Code Alarm", `A response has returned an unsuccessfull status code.\n \n\`\`\`\n${response.status} ${response.statusText}\n\`\`\`\`\`\`\n${text}\n\`\`\`\n${request.method} ${request.url}\nRemote Address: || ${request.headers.get("CF-Connecting-IP")} ||`);
-                    else
-                        await triggerAlarm(env, "Unsuccessful Status Code Alarm", `A response has returned an unsuccessfull status code.\n \n\`\`\`\n${response.status} ${response.statusText}\n\`\`\`\n${request.method} ${request.url}\nRemote Address: || ${request.headers.get("CF-Connecting-IP")} ||`);
+                    await triggerAlarm(env, "Unsuccessful Status Code Alarm", `A response has returned an unsuccessfull status code.\n \n\`\`\`\n${response.status} ${response.statusText}\n\`\`\`${(text.length)?(`\`\`\`\n${text}\n\`\`\``):("")}\n${request.method} ${request.url}\nRemote Address: || ${request.headers.get("CF-Connecting-IP")} ||`);
+             
+                    resolve();
                 }));
+            }
+
+            response.headers.set("Access-Control-Allow-Origin", "*");
+            response.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
+            response.headers.set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
             
             return response;
         }
         catch(error: any) {
-            if((error as Error).message.startsWith("D1_")) {
-                const d1Error: D1Error = error;
-
-                context.waitUntil(triggerAlarm(env, "D1 Error Alarm", `An error was thrown by D1 during execution.\n \n\`\`\`\n${d1Error.message}\n\`\`\`\`\`\`\n${d1Error.cause.message}\n\`\`\`\n${request.method} ${request.url}\nRemote Address: || ${request.headers.get("CF-Connecting-IP")} ||`));
-            
-                return new Response(undefined, {
-                    status: 502,
-                    statusText: "Bad Gateway"
-                });
+            if(error instanceof Error) {
+                if(error.message.startsWith("D1_") && error.cause instanceof Error) {
+                    context.waitUntil(triggerAlarm(env, "D1 Error Alarm", `An error was thrown by D1 during execution.\n \n\`\`\`\n${error.message}\n\`\`\`\`\`\`\n${error.cause.message}\n\`\`\`\n${request.method} ${request.url}\nRemote Address: || ${request.headers.get("CF-Connecting-IP")} ||`));
+                
+                    return new Response(undefined, {
+                        status: 502,
+                        statusText: "Bad Gateway"
+                    });
+                }
             }
 
             context.waitUntil(triggerAlarm(env, "Uncaught Error Alarm", `An uncaught error was thrown during a response.\n \n\`\`\`\n${error}\n\`\`\`\n${request.method} ${request.url}\nRemote Address: || ${request.headers.get("CF-Connecting-IP")} ||`));
--- src/models/D1Error.ts
+++
@@ -1,3 +0,0 @@
-export default interface D1Error extends Error {
-    cause: D1Error;
-};
