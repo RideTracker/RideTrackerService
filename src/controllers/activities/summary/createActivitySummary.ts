148d70947b0c33504cb8120e1a7389a07c4f9bd2
Nora Söderlund <nora.soderlund@polestar.com>
2023-07-04 22:31:32 +0200

    Fixed activity summary query

--- src/controllers/activities/summary/createActivitySummary.ts
+++ src/controllers/activities/summary/createActivitySummary.ts
@@ -3,5 +3,5 @@ import { ActivitySummary } from "../../../models/activitySummary";
 export async function createActivitySummary(database: D1Database, id: string, key: string, value: number): Promise<void> {
     const timestamp = Date.now();
 
-    await database.prepare("INSERT INTO activity_summary (id, key, value, personal_best, timestamp) VALUES (?, ?, ?, ?, ?)").bind(id, key, value, false, timestamp).run();
+    await database.prepare("INSERT INTO activity_summary (id, key, value, personal_best, timestamp) VALUES (?, ?, ?, ?, ?)").bind(id, key, value, 0, timestamp).run();
 };
