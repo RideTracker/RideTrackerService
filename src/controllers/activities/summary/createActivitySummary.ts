4b46338352f91c21b449e078eafa467b2d670173
Nora Söderlund <nora.soderlund@polestar.com>
2023-07-04 22:15:28 +0200

    Fixed activity summary

--- src/controllers/activities/summary/createActivitySummary.ts
+++ src/controllers/activities/summary/createActivitySummary.ts
@@ -1,7 +1,7 @@
 import { ActivitySummary } from "../../../models/activitySummary";
 
-export async function createActivitySummary(database: D1Database, id: string, key: string, value: number): Promise<ActivitySummary> {
+export async function createActivitySummary(database: D1Database, id: string, key: string, value: number): Promise<void> {
     const timestamp = Date.now();
 
-    return database.prepare("INSERT INTO activity_summary (id, key, value, personal_best, timestamp) VALUES (?, ?, ?, ?, ?) RETURNING *").bind(id, key, value, false, timestamp).first<ActivitySummary>();
+    await database.prepare("INSERT INTO activity_summary (id, key, value, personal_best, timestamp) VALUES (?, ?, ?, ?, ?) RETURNING *").bind(id, key, value, false, timestamp).run();
 };
--- src/controllers/activities/updateActivityAreas.ts
+++ src/controllers/activities/updateActivityAreas.ts
@@ -1,5 +1,5 @@
 import { Activity } from "../../models/activity";
 
-export async function updateActivityAreas(database: D1Database, id: string, startArea: string | null, finishArea: string | null): Promise<Activity> {
-    return await database.prepare("UPDATE activities SET start_area = ?, finish_area = ? WHERE id = ? RETURNING *").bind(startArea, finishArea, id).first<Activity>();
+export async function updateActivityAreas(database: D1Database, id: string, startArea: string | null, finishArea: string | null): Promise<void> {
+    await database.prepare("UPDATE activities SET start_area = ?, finish_area = ? WHERE id = ?").bind(startArea, finishArea, id).run();
 };
