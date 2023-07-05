f9dd1a09f73cb54fb900632ffd97392a04663914
Nora Söderlund <nora.soderlund@polestar.com>
2023-07-05 12:59:21 +0200

    Added activity summary key type

---
+++ src/models/ActivitySummaryKey.ts
@@ -0,0 +1,1 @@
+export type ActivitySummaryKey = "max_speed" | "average_speed" | "distance" | "elevation";
--- src/models/activitySummary.ts
+++ src/models/activitySummary.ts
@@ -1,6 +1,8 @@
+import { ActivitySummaryKey } from "./ActivitySummaryKey";
+
 export type ActivitySummary = {
     id: string;
-    key: string;
+    key: ActivitySummaryKey;
     value: number;
     personalBest: boolean;
     timestamp: number;
