dcb8296792e4520ae0cac427a38a283e53c2de07
Nora Söderlund <nora.soderlund@polestar.com>
2023-07-05 13:17:58 +0200

    Fixed activity summary query

--- src/controllers/activities/summary/getActivitySummaryById.ts
+++ src/controllers/activities/summary/getActivitySummaryById.ts
@@ -2,5 +2,5 @@ import { Activity } from "../../../models/activity";
 import { ActivitySummary } from "../../../models/activitySummary";
 
 export async function getActivitySummaryById(database: D1Database, id: string): Promise<ActivitySummary[]> {
-    return (await database.prepare("SELECT * FROM activity_summary WHERE id = ?").bind(id).all<ActivitySummary>()).results ?? [];
+    return (await database.prepare("SELECT personal_best AS personalBest, activity_summary.* FROM activity_summary WHERE id = ?").bind(id).all<ActivitySummary>()).results ?? [];
 };
