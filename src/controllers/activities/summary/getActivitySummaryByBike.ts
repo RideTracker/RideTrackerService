2e22dd19134b6fb02a378c13ab0b555a002fff1f
Nora Söderlund <nora.soderlund@polestar.com>
2023-07-06 16:58:53 +0200

    Fixed bike summary query order

--- src/controllers/activities/summary/getActivitySummaryByBike.ts
+++ src/controllers/activities/summary/getActivitySummaryByBike.ts
@@ -1,5 +1,5 @@
 import { ActivitySummary } from "../../../models/activitySummary";
 
 export async function getActivitySummaryByBike(database: D1Database, bikeId: string): Promise<ActivitySummary[]> {
-    return (await database.prepare("SELECT key, value FROM (SELECT activity_summary.key AS key, SUM(activity_summary.value) AS value FROM activity_summary INNER JOIN activities ON activities.bike = ?1 WHERE activity_summary.id = activities.id GROUP BY activity_summary.key UNION ALL SELECT 'activities' AS key, COUNT(id) AS value FROM activities WHERE bike = ?1)").bind(bikeId).all<ActivitySummary>()).results ?? [];
+    return (await database.prepare("SELECT key, value FROM (SELECT 'activities' AS key, COUNT(id) AS value FROM activities WHERE bike = ?1 UNION ALL SELECT activity_summary.key AS key, SUM(activity_summary.value) AS value FROM activity_summary INNER JOIN activities ON activities.bike = ?1 WHERE activity_summary.id = activities.id GROUP BY activity_summary.key)").bind(bikeId).all<ActivitySummary>()).results ?? [];
 };
