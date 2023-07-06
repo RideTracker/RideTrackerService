e4ef0f3201b4866b8137a1f1d5ecf41860188a74
Nora Söderlund <nora.soderlund@polestar.com>
2023-07-06 16:55:09 +0200

    Updated activity bike summary

--- src/controllers/activities/getActivityCountByBike.ts
+++
@@ -1,3 +0,0 @@
-export async function getActivityCountByBike(database: D1Database, bikeId: string): Promise<number> {
-    return await database.prepare("SELECT COUNT(*) AS count FROM activities WHERE bike = ?").bind(bikeId).first<number>("count");
-};
