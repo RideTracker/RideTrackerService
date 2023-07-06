9cc5860345f4eb107d71b6fd6ffa020f76ebba06
Nora Söderlund <nora.soderlund@polestar.com>
2023-07-06 14:35:12 +0200

    Fixed bike activity summary

--- src/controllers/activities/summary/getActivitySummaryByBike.ts
+++ src/controllers/activities/summary/getActivitySummaryByBike.ts
@@ -1,5 +1,5 @@
 import { ActivitySummary } from "../../../models/activitySummary";
 
 export async function getActivitySummaryByBike(database: D1Database, bikeId: string): Promise<ActivitySummary[]> {
-    return await database.prepare("SELECT activity_summary.key, SUM(activity_summary.value) FROM activity_summary INNER JOIN activities ON activities.bike = ? WHERE activity_summary.id = activities.id GROUP BY activity_summary.key").bind(bikeId).first();
+    return (await database.prepare("SELECT activity_summary.key, SUM(activity_summary.value) FROM activity_summary INNER JOIN activities ON activities.bike = ? WHERE activity_summary.id = activities.id GROUP BY activity_summary.key").bind(bikeId).all<ActivitySummary>()).results ?? [];
 };
