657da42e0543eb8791c99d8b0fbd3be972341333
Nora Söderlund <norasoderlund@icloud.com>
2023-07-30 20:04:58 +0200

    Fixed get devices by user

--- src/controllers/devices/getDevicesByUser.ts
+++ src/controllers/devices/getDevicesByUser.ts
@@ -1,5 +1,5 @@
 import { Device } from "../../models/Device";
 
 export async function getDevicesByUser(database: D1Database, userId: string): Promise<Device[]> {
-    return (await database.prepare("SELECT * FROM devices WHERE id = ? ORDER BY timestamp DESC").bind(userId).all<Device>()).results ?? [];
+    return (await database.prepare("SELECT * FROM devices WHERE user = ? ORDER BY timestamp DESC").bind(userId).all<Device>()).results ?? [];
 };
